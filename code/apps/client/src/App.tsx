ded4d6a0c117f29ebe1451039c5b3a1f556bfb5a
Oriel Romero <orielromero97@gmail.com>
2026-08-03 00:10:12 -0300

    fix(ci-cd-staging): added build step in workflow to deploy

--- code/apps/client/src/App.tsx
+++ code/apps/client/src/App.tsx
@@ -7,6 +7,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   // Comentario temporal para probar workflow
+  // otro comentario
   return (
     <>
       <section id="center">
